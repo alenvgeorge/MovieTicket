38ae243a1797ae9bfdca873fa5da6febe3ada543
alenvgeorge <alenvgeorge98@gmail.com>
2020-10-06 20:20:12 +0530

    Update movie.h

--- movie/inc/movie.h
+++ movie/inc/movie.h
@@ -1,4 +1,4 @@
-/** 
+/**
 * @file movie.h
 * Movie Ticket Booking Application
 *
@@ -6,6 +6,7 @@
 #ifndef __MOVIE_H__
 #define __MOVIE_H__
 
+/// @private
 struct movie_details
 {
     int movie_code;
@@ -16,25 +17,82 @@ struct movie_details
     int seat_no;
 };
 
+
 extern struct movie_details m;
 
 struct movie_details m;
 
 /**
 *  Function to validate admin login
-* @param[in] admin_username 
+* @param[in] admin_username
 * @param[in] admin_password
-* @return Pass/Fail status of login
+* @return Pass/Fail status of admin login
 */
 int admin_login(char *admin_username, char *admin_password);
-char* add_movies(int code, char *name, char *timing, char *date, int price, int seat);
-char* user_reg(char *fname, char*uname, char *pass, char*mob_no);
-char* user_login(char *username, char *password);
-int view_movies();
-char *book_tickets(char*movie, int seat);
-char* search(char* m_name);
-char *view_booked();
+/**
+*  Function for admin to add movies
+* @param[in] movie_code
+* @param[in] movie_name
+* @param[in] show_timing
+* @param[in] release_date
+* @param[in] ticket_price
+* @param[in] seats
+* @return "Movie Details Added Successfully" if the details are successfully stored in file.
+*/
+
+char* add_movies(int movie_code, char *movie_name, char *show_timing, char *release_date, int ticket_price, int seats);
+/**
+*  Function for registration of new users
+* @param[in] fullname
+* @param[in] username
+* @param[in] password
+* @param[in] mobile_no
+* @return "User Registration Successful" if user details are stored successfully
+*/
+char* user_reg(char *fullname, char*username, char *password, char*mobile_no);
+
+/**
+*  Function for admin to view all the bookings of the theatre
+* @return All Tickets and summary
+*/
 int view_bookings();
+
+
+
+/**
+*  Function for login of existing users
+* @param[in] username
+* @param[in] username
+* @return Pass/Fail status of user login
+*/
+char* user_login(char *username, char *password);
+
+/**
+*  Function for users to view the list of movies
+* @return list of movies added by the admin
+*/
+int view_movies();
+
+/**
+*  Function for users to book movie tickets
+* @param[in] movie
+* @param[in] seat
+* @return Ticket for the movie containing the details of the show as well as the user
+*/
+char *book_tickets(char*movie, int seat);
+
+/**
+*  Function for users to search movie by name
+* @param[in] m_name
+* @return Details of movie if available
+*/
+char* search(char* m_name);
+
+/**
+*  Function for users to view the booked tickets
+* @return Tickets if the user has booked tickets
+*/
+char *view_booked();
 
 
 #endif // __MOVIE_H__
